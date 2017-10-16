31b4cad078490bf244917ff3967952e5e0ed54a2
mu-ci <cplee+mu@nektos.com>
2017-10-16 22:39:46 +0000

    auto updated the mu-cli formula for version v1.0.5 branch master

--- Formula/mu-cli.rb
+++ Formula/mu-cli.rb
@@ -3,13 +3,13 @@ class MuCli < Formula
   desc "A full-stack DevOps on AWS tool"
   homepage "https://getmu.io"
   if OS.mac?
-    url "https://github.com/stelligent/mu/releases/download/v1.0.4/mu-darwin-amd64" # The MacOS master url
-    sha256 "558b41f106734d3c6ac505fb9ee2c3c0ad3cc34602d21f6b2848efa643d531a5" # The MacOS master sha256sum
+    url "https://github.com/stelligent/mu/releases/download/v1.0.5/mu-darwin-amd64" # The MacOS master url
+    sha256 "89d4ef6be48a19b73a24a078b0c4457640cd6e65576c54746d8ce8e804c4cdd7" # The MacOS master sha256sum
   elsif OS.linux?
-    url "https://github.com/stelligent/mu/releases/download/v1.0.4/mu-linux-amd64" # The Linux master url
-    sha256 "01cfe1cfafbf13e873f423492eb17140f7cfc1a4d23d7e89c275caf8a7817626" # The Linux master sha256sum
+    url "https://github.com/stelligent/mu/releases/download/v1.0.5/mu-linux-amd64" # The Linux master url
+    sha256 "5b7ca88339ff7a51bc3c85cadd6377d795171c638ff7237aa1fe806b276fce1e" # The Linux master sha256sum
   end
-  version "1.0.4" # The master version
+  version "1.0.5" # The master version
   devel do
     if OS.mac?
     url "https://github.com/stelligent/mu/releases/download/v1.1.1-develop/mu-darwin-amd64" # The MacOS develop url
