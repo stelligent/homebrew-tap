0a3c5aa2a05693a428d3f709765f558d87a200eb
mu-ci <cplee+mu@nektos.com>
2017-10-17 05:57:53 +0000

    auto updated the mu-cli formula for version v1.1.1-develop branch develop

--- Formula/mu-cli.rb
+++ Formula/mu-cli.rb
@@ -13,10 +13,10 @@ class MuCli < Formula
   devel do
     if OS.mac?
     url "https://github.com/stelligent/mu/releases/download/v1.1.1-develop/mu-darwin-amd64" # The MacOS develop url
-    sha256 "cd79f0e422cb9d421c1a19703e890c4d3d94de7a4563fff760a00a254b04262b" # The MacOS develop sha256sum
+    sha256 "9219f627b08445383504cdb14479fee00ae08189f85a5be40ed3b1ee2277925c" # The MacOS develop sha256sum
     elsif OS.linux?
     url "https://github.com/stelligent/mu/releases/download/v1.1.1-develop/mu-linux-amd64" # The Linux develop url
-    sha256 "4e474c92df879149c188b8dbd2be5a8e2b73dc9bee591d6ece3a3a5bf5f5891b" # The Linux develop sha256sum
+    sha256 "8f75bbea0cbb7a07623f018c910793d3e26fb96a12c842b772c668a00bd18826" # The Linux develop sha256sum
     end
     version "1.1.1-develop" # The develop version
   end
