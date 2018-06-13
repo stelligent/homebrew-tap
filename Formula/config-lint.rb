f5cb1c65b4cd8f0368f6737e3940fccf827c2d53
goreleaserbot <goreleaser@stelligent.com>
2018-06-13 10:59:57 -0700

    Brew formula update for config-lint version v0.0.7

--- Formula/config-lint.rb
+++ Formula/config-lint.rb
@@ -1,9 +1,9 @@
 class ConfigLint < Formula
   desc ""
   homepage ""
-  url "https://github.com/stelligent/config-lint/releases/download/v0.0.5/config-lint_0.0.5_Darwin_x86_64.tar.gz"
-  version "0.0.5"
-  sha256 "07e0357aa6c80399880226442dde70fd36516aa54b2b7ee6c1fec2ddb4a937d3"
+  url "https://github.com/stelligent/config-lint/releases/download/v0.0.7/config-lint_0.0.7_Darwin_x86_64.tar.gz"
+  version "0.0.7"
+  sha256 "92ff6fb1252b060cb1aa7076c46c8b5289ad0678029420ba4126b4a43cce1bf5"
 
   def install
     bin.install "config-lint"
