ed283929055d45174be31bfa31e37d8349ed6156
KLEstes <klestes@gmail.com>
2019-07-16 23:49:27 -0500

    Removed extraneous comments.

--- Formula/cfn-nag.rb
+++ Formula/cfn-nag.rb
@@ -55,15 +55,6 @@ class CfnNag < Formula
 
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test cfn_nag`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
 
     assert_equal version, shell_output("#{bin}/cfn_nag -v").strip
     assert_equal version, shell_output("#{bin}/cfn_nag_scan -v").strip
