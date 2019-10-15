0827035255e9c1d41dbd72b36802b8ce07bb94e2
goreleaserbot <goreleaser@stelligent.com>
2019-10-15 13:44:32 -0500

    Brew formula update for config-lint version v0.0.39

--- Formula/config-lint.rb
+++ Formula/config-lint.rb
@@ -2,16 +2,16 @@
 class ConfigLint < Formula
   desc ""
   homepage ""
-  version "0.0.38"
+  version "0.0.39"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/stelligent/config-lint/releases/download/v0.0.38/config-lint_0.0.38_Darwin_x86_64.tar.gz"
-    sha256 "15e8ac11f87926f55839f90e1975a3dc086f6770ad49e9341c507f6c1ce6882b"
+    url "https://github.com/stelligent/config-lint/releases/download/v0.0.39/config-lint_0.0.39_Darwin_x86_64.tar.gz"
+    sha256 "0a9e6704c5f5713c521a37fdd231fb6124e1ad43c5837391b563336edcd1dce7"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/stelligent/config-lint/releases/download/v0.0.38/config-lint_0.0.38_Linux_x86_64.tar.gz"
-      sha256 "04d11203c53af93bd3fc776a70dff0a5609ca7b65b58232fcadfc5b1d2c48030"
+      url "https://github.com/stelligent/config-lint/releases/download/v0.0.39/config-lint_0.0.39_Linux_x86_64.tar.gz"
+      sha256 "ce563a929e7f48fe8aa24dd3cf3cba163e090c1bab0c3883e0921d3593d62cb2"
     end
   end
 
