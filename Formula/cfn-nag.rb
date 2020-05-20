0e64293e1ea1d3524c2f6948869359944a5a20d4
stelligent-homebrew-tap-updater <homebrew-tap@stelligent.com>
2020-05-20 18:27:50 +0000

    auto-update cfn_nag version (v0.6.2) and sha256

--- Formula/cfn-nag.rb
+++ Formula/cfn-nag.rb
@@ -2,9 +2,9 @@ class CfnNag < Formula
 
   desc "Auditing tool for CloudFormation templates"
   homepage "https://github.com/stelligent/cfn_nag"
-  url "https://github.com/stelligent/cfn_nag/tarball/v0.6.1"
-  version "0.6.1"
-  sha256 "49b5b3f213bd7b792eacf447e6ecd0c926fbd4b5bea1b325f7b95fa7e943f067"
+  url "https://github.com/stelligent/cfn_nag/tarball/v0.6.2"
+  version "0.6.2"
+  sha256 "e038edf1ffa2a0c6c8f0da551dc0f5f6eacb19093348c30957a13ed7d486b5be"
 
 
   depends_on "ruby" if MacOS.version <= :sierra
